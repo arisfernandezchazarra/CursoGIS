eb8768e0ef96f07fc470adcaa903f90eaf4d3fb6
arisfernandezchazarra <arisfernandezchazarra@gmail.com>
2026-04-28 12:56:01 +0200

    Curso Pyhon

---
+++ 00_hello.py
@@ -0,0 +1,25 @@
+# Esto es un comentario: Hola Mundo
+# Nuestro Hola Mundo en Python
+
+print("Hola Python")
+print('Hola Python')
+
+"""
+Este es
+un comentario
+en varias líneas 
+"""
+
+'''
+Esto también es un comentario
+'''
+
+
+# Consultar el tipo de dato
+print (type("Soy un dato str")) # Tipo 'Str o String'
+print (type(5)) # Tipo 'Int'
+print (type (1.5)) # TIpo 'float'
+print (type (1 + 2j)) # Tipo 'complex'
+print (type (True)) # Tipo 'bool'
+
+
